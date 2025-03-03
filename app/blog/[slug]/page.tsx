b7a5afcfb7d38146a58bf5140f95a7b6a18afb5b
Nguyenbakhoa-sacly <khoana23062002@gmail.com>
2025-03-03 16:55:01 +0700

    update1

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -9,5 +9,9 @@ export async function generateMetadata({ params }: Params) {
 }
 
 export default function Page({ params }: Params) {
-  return <><h1>Slug: {params.slug}</h1><p>Hello dev</p></>;
+  return <>
+    <h1>Slug: {params.slug}</h1>
+    <p>Hello dev</p>
+    <p>Chào mừng bạn đến với Đà Nẵng</p>
+  </>;
 }
